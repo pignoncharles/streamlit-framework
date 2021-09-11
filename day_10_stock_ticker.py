44c65481a6994d27716535502bcc9ddecb05dd54
Charles Pignon <charles.pignon1@gmail.com>
2021-09-10 19:38:20 -0700

    Added_stock_ticker_app
    First demo version of app: works if provided API key

---
+++ day_10_stock_ticker.py
@@ -0,0 +1,66 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+import time
+
+import matplotlib.pyplot as plt
+from alpha_vantage.timeseries import TimeSeries
+
+import altair as alt
+
+
+import requests
+
+st.title('Pignon 2021 Data Incubator Interactive Stock Ticker')
+st.write("Display Google stock daily closing price for a user-selected month & year:")
+
+user_input_stock_name = st.sidebar.text_input("Select a stock (e.g. GOOG, TSLA)", 'GOOG')
+
+
+ts = TimeSeries(key, output_format='pandas')
+# data, meta = ts.get_intraday('TSLA', interval='1min', outputsize='full')
+# data, meta = ts.get_daily('GOOG', outputsize='full')
+data, meta = ts.get_daily(user_input_stock_name, outputsize='full')
+
+st.write("Stock ticker metadata:")
+st.write(meta)
+
+# st.write("Stock dataset info:")
+# st.write(data.info())
+
+# st.write("Stock dataset head:")
+# st.write(data.head())
+
+#extract year month
+data['year'] = data.index.year
+data['month'] = data.index.month
+
+#year and month selection
+year_option = st.sidebar.selectbox(
+    'Select a year',
+     set(data['year']))
+
+month_option = st.sidebar.selectbox(
+    'Select a month',
+     set(data['month']))
+
+'You selected: ', month_option, year_option
+
+data=data[data.index.year == year_option]
+data=data[data.index.month == month_option]
+
+#keep only closing data
+data=data[['4. close']]
+
+
+
+# plot the graph
+chart = st.line_chart(data)
+
+
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+st.button("Re-run")
